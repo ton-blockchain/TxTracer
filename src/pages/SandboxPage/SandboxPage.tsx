0f0bef5506d3cc47c9f897c7352a2250db9d757b
i582 <51853996+i582@users.noreply.github.com>
2025-06-17 00:27:27 +0400

    more data

--- src/pages/SandboxPage/SandboxPage.tsx
+++ src/pages/SandboxPage/SandboxPage.tsx
@@ -28,7 +28,7 @@ import styles from "./SandboxPage.module.css"
 
 export type TransactionRawInfo = {
   readonly transaction: string
-  readonly fields: object
+  readonly fields: Record<string, unknown>
   readonly parentId: string
   readonly childrenIds: string[]
 }
@@ -39,14 +39,14 @@ export type TransactionsInfo = {
 
 export type TransactionInfoRaw = {
   readonly transaction: Transaction
-  readonly fields: object
+  readonly fields: Record<string, unknown>
   readonly parentId: string
   readonly childrenIds: string[]
 }
 
 export type TransactionInfo = {
   readonly transaction: Transaction
-  readonly fields: object
+  readonly fields: Record<string, unknown>
   readonly parent: TransactionInfo | undefined
   readonly children: TransactionInfo[]
 }
@@ -97,13 +97,20 @@ type TestData = {
   readonly transactions: TransactionInfo[]
 }
 
-function TestFlow({
+function TransactionShortInfo({
+  tx,
   contracts,
-  testData,
 }: {
+  tx: TransactionInfo
   contracts: Map<string, ContractData>
-  testData: TestData
 }) {
+  if (tx.transaction.description.type !== "generic") {
+    throw new Error(
+      "TxTracer doesn't support non-generic transaction. Given type: " +
+        tx.transaction.description.type,
+    )
+  }
+
   const formatAddress = (
     address: Address | Maybe<ExternalAddress> | undefined,
   ): React.ReactNode => {
@@ -129,6 +136,62 @@ function TestFlow({
     }
   }
 
+  const computePhase = tx.transaction.description.computePhase
+  const computeInfo =
+    computePhase.type === "skipped"
+      ? "skipped"
+      : {
+          success: computePhase.success,
+          exitCode:
+            computePhase.exitCode === 0
+              ? (tx.transaction.description.actionPhase?.resultCode ?? 0)
+              : computePhase.exitCode,
+          vmSteps: computePhase.vmSteps,
+          gasUsed: computePhase.gasUsed,
+          gasFees: computePhase.gasFees,
+        }
+
+  const logs = tx.fields["vmLogs"] as string
+  const blockchainLogs = tx.fields["blockchainLogs"] as string
+  const debugLogs = tx.fields["debugLogs"] as string
+
+  return (
+    <div>
+      <div>lt: {tx.transaction.lt}</div>
+      <div>this: {formatAddress(bigintToAddress(tx?.transaction?.address)) ?? "no parent"}</div>
+      <div>parent tx: {tx.parent?.transaction?.lt ?? "unknown"}</div>
+      <div>
+        in msg: {formatAddress(tx.transaction.inMessage?.info?.src)}
+        {" -> "}
+        {formatAddress(tx.transaction.inMessage?.info?.dest)}
+      </div>
+      <div>out: {tx.transaction.outMessagesCount}</div>
+      {computeInfo === "skipped" ? (
+        <div>skipped</div>
+      ) : (
+        <div>
+          <div>success: {(computeInfo?.success ?? false) ? "true" : "false"}</div>
+          <div>exit code: {computeInfo?.exitCode}</div>
+          <div>vmSteps: {computeInfo?.vmSteps}</div>
+          <div>gasUsed: {computeInfo?.gasUsed}</div>
+          <div>gasFees: {computeInfo?.gasFees}</div>
+          <div>vmLogs: {logs.slice(0, Math.min(100, logs.length))}</div>
+          <div>blockchainLogs: {blockchainLogs.slice(0, Math.min(100, blockchainLogs.length))}</div>
+          <div>debugLogs: {debugLogs.slice(0, Math.min(100, debugLogs.length))}</div>
+        </div>
+      )}
+      <br />
+    </div>
+  )
+}
+
+function TestFlow({
+  contracts,
+  testData,
+}: {
+  contracts: Map<string, ContractData>
+  testData: TestData
+}) {
   const transactions = testData.transactions.filter(it => {
     return (
       it.transaction.inMessage?.info?.src?.toString() !==
@@ -142,18 +205,7 @@ function TestFlow({
         {testData.testName ?? "unknown test"}: {testData.id}
       </div>
       {transactions.map((tx, index) => (
-        <div key={index}>
-          <div>lt: {tx.transaction.lt}</div>
-          <div>this: {formatAddress(bigintToAddress(tx?.transaction?.address)) ?? "no parent"}</div>
-          <div>parent tx: {tx.parent?.transaction?.lt ?? "unknown"}</div>
-          <div>
-            in msg: {formatAddress(tx.transaction.inMessage?.info?.src)}
-            {" -> "}
-            {formatAddress(tx.transaction.inMessage?.info?.dest)}
-          </div>
-          <div>out: {tx.transaction.outMessagesCount}</div>
-          <br />
-        </div>
+        <TransactionShortInfo key={index} tx={tx} contracts={contracts} />
       ))}
     </>
   )
