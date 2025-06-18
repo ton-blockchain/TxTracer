d72117f632799fd616eb6ca54486181da0582a38
i582 <51853996+i582@users.noreply.github.com>
2025-06-19 03:34:17 +0400

    Updates

--- assets/index-CQ9_rxVT.js
+++
@@ -1,1 +0,0 @@
-import{a as r,b as t}from"./ton-assembly-test-dev-GzvYfkSs.js";var a=r(),e=t();export{a as i,e as t};
--- assets/index-INGO7O_s.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./ton-assembly-test-dev-GzvYfkSs.js";var t=r();export{t as d};
