388b8dc3e108dc901f63d963980ff66ce65511d5
Petr Makhnev <51853996+i582@users.noreply.github.com>
2025-08-20 22:36:17 +0400

    feat(spec): add control register to details and card to main page (#96)

---
+++ src/pages/InstructionsPage/components/InstructionTable/RegisterSquare.tsx
@@ -0,0 +1,37 @@
+import React from "react"
+
+import styles from "@app/pages/InstructionsPage/components/InstructionTable/RegisterSquare.module.css"
+import {CONTROL_REGISTERS} from "@features/common/lib/control-registers/control-registers.ts"
+import {Tooltip} from "@shared/ui/Tooltip"
+
+export interface RegisterSquareProps {
+  readonly index?: number
+  readonly variable?: string
+}
+
+export const RegisterSquare: React.FC<RegisterSquareProps> = ({index, variable}) => {
+  const className =
+    index === 0
+      ? styles.c0
+      : index === 1
+        ? styles.c1
+        : index === 3
+          ? styles.c3
+          : index === 7
+            ? styles.c7
+            : variable === "i"
+              ? styles.ci
+              : ""
+
+  const crInfo = CONTROL_REGISTERS[`c${index}`]
+  const tooltipContent = <p>{crInfo?.description ?? "Unknown control register"}</p>
+
+  return (
+    <Tooltip content={tooltipContent} variant="hover">
+      <div className={styles.registerSquare + " " + className}>
+        {index !== undefined && <span className={styles.registerName}>c{index}</span>}
+        {variable !== undefined && <span className={styles.registerName}>c({variable})</span>}
+      </div>
+    </Tooltip>
+  )
+}
