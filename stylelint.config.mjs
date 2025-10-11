1b343ddf115546240afcfca7f310928046e3e47d
Danil Ovchinnikov <danil42worker@gmail.com>
2025-10-11 13:44:16 +0300

    fix rules `value-keyword-case`

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -12,5 +12,12 @@ export default {
     "custom-property-empty-line-before": null,
     "declaration-empty-line-before": null,
     "comment-empty-line-before": null,
+
+    "value-keyword-case": [
+      "lower",
+      {
+        ignoreProperties: ["composes", "compose-with"],
+      },
+    ],
   },
 }
