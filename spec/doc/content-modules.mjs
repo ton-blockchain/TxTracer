7c5423cf78c9f7aa38588fe2dca8da5a75d0caf1
i582 <51853996+i582@users.noreply.github.com>
2025-10-13 13:23:02 +0400

    Updates

--- spec/doc/content-modules.mjs
+++ spec/doc/content-modules.mjs
@@ -1,6 +1,6 @@
 
 export default new Map([
 ["src/content/docs/book/continuations/basics-register-c0-cc-savelist-if-instruction.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fbook%2Fcontinuations%2Fbasics-register-c0-cc-savelist-if-instruction.mdx&astroContentModuleFlag=true")],
-["src/content/docs/book/continuations/diving-deeper-exit-points-of-continuations.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fbook%2Fcontinuations%2Fdiving-deeper-exit-points-of-continuations.mdx&astroContentModuleFlag=true")],
-["src/content/docs/book/continuations/manual-handling-and-jmp-vs-execute.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fbook%2Fcontinuations%2Fmanual-handling-and-jmp-vs-execute.mdx&astroContentModuleFlag=true")]]);
+["src/content/docs/book/continuations/manual-handling-and-jmp-vs-execute.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fbook%2Fcontinuations%2Fmanual-handling-and-jmp-vs-execute.mdx&astroContentModuleFlag=true")],
+["src/content/docs/book/continuations/diving-deeper-exit-points-of-continuations.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fbook%2Fcontinuations%2Fdiving-deeper-exit-points-of-continuations.mdx&astroContentModuleFlag=true")]]);
 		
