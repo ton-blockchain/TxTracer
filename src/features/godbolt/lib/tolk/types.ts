f45dd29d3bd3680b7b27c81d0eb5093f88c91955
Danil Ovchinnikov <danil42worker@gmail.com>
2025-12-05 23:52:13 +0300

    fix code

--- src/features/godbolt/lib/tolk/types.ts
+++ src/features/godbolt/lib/tolk/types.ts
@@ -1,11 +1,12 @@
-import {runtime as i, trace} from "ton-assembly"
+import {runtime as i} from "ton-assembly"
+import type {InstructionInfo} from "ton-source-map"
 
 export interface TolkCompilationResult {
   readonly lang: "tolk"
   readonly instructions: i.Instr[]
   readonly code: string
   readonly assembly: string
-  readonly mapping: Map<number, trace.InstructionInfo[]>
+  readonly mapping: Map<number, InstructionInfo[]>
 }
 
 export class TolkCompilationError extends Error {
